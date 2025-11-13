03738c994ee7f533a7cf1ed36ad4e07e03dd81a4
birendra rai <birendrakumar98p@gmail.com>
2025-11-13 14:15:35 +0545

    fisrst commit for publish file

--- obj/Release/net8.0/FoodMenu.AssemblyInfo.cs
+++ obj/Release/net8.0/FoodMenu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodMenu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d55f4102227e75dfa3b27b505c23bb8b58b4381")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodMenu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodMenu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
